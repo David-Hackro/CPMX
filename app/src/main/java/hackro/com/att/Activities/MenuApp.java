c471595797917a5e34e60b59ac8331b82de39ea7
David Hackro <hackro91@gmail.com>
2016-01-02 15:16:42 -0600

    version 1.0

---
+++ app/src/main/java/hackro/com/att/Activities/MenuApp.java
@@ -0,0 +1,17 @@
+package hackro.com.att.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hackro.com.att.R;
+
+public class MenuApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_app);
+
+        
+    }
+}
